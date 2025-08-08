c78dcf4e9668402d500a68abc8d38cd3cd43322c
Tuấn Thịnh <ltthinh111@gmail.com>
2025-08-08 12:43:11 +0700

    Implement new features: add cart, checkout, admin authentication (login/logout functionality) and dashboard components.

---
+++ src/components/admin/Login.jsx
@@ -0,0 +1,106 @@
+import React, { useContext } from "react";
+import Layout from "../common/Layout";
+import { useForm } from "react-hook-form";
+import { apiUrl } from "../common/http";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { AdminAuthContext } from "../context/AdminAuth";
+
+const Login = () => {
+  const {login} = useContext(AdminAuthContext);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+
+    const res = await fetch(apiUrl + "/admin/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          const adminInfo = {
+            token: result.token,
+            id: result.id,
+            name: result.name,
+          };
+
+          localStorage.setItem("adminInfo", JSON.stringify(adminInfo));
+          login(adminInfo);
+          toast.success("Login successful!");
+          navigate("/admin/dashboard");
+        } else {
+          toast.error("Login failed. Please try again.");
+        }
+      });
+  };
+  return (
+    <Layout>
+      <div className="container d-flex justify-content-center align-items-center py-5">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="card shadow border-0 login">
+            <div className="card-body p-4">
+              <h3 className="text-center">
+                <strong>Admin Login</strong>
+              </h3>
+              <div className="mb-3">
+                <label htmlFor="" className="form-label">
+                  Email
+                </label>
+                <input
+                  {...register("email", {
+                    required: "The email field is required.",
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+                      message: "Please enter a valid email address.",
+                    },
+                  })}
+                  type="text"
+                  className={`form-control ${errors.email && "is-invalid"}`}
+                  placeholder="Email"
+                />
+                {errors.email && (
+                  <p className="invalid-feedback">{errors.email?.message}</p>
+                )}
+              </div>
+              <div className="mb-3">
+                <label htmlFor="" className="form-label">
+                  Password
+                </label>
+                <input
+                  {...register("password", {
+                    required: "The password field is required.",
+                  })}
+                  type="password"
+                  className={`form-control ${errors.password && "is-invalid"}`}
+                  placeholder="Password"
+                />
+                {errors.password && (
+                  <p className="invalid-feedback">{errors.password?.message}</p>
+                )}
+              </div>
+              <div className="d-flex justify-content-center">
+                <button className="btn btn-secondary">Login</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
